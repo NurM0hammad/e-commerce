a658eece78a751f04b6945ebcdf306cb544f673e
Nur <nur.mohammad.cyber@gmail.com>
2026-02-26 03:12:00 +0600

    Fixed static configuration

--- shopwave/settings.py
+++ shopwave/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.staticfiles'
     'apps.core',
     'apps.products',
     'apps.cart',
@@ -35,6 +34,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
